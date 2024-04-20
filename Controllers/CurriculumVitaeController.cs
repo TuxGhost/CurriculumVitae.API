f291151b5b3777af4cc4a4bad7a5cc5b17ae5de0
Peter Kuda <petar@outlook.be>
2024-04-20 18:38:34 +0200

    First method

--- Controllers/CurriculumVitaeController.cs
+++ Controllers/CurriculumVitaeController.cs
@@ -1,10 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CurriculumVitae.common.Services;
+using CurriculumVitaeRepository.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace CurriculumVitae.API.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class CurriculumVitaeController : ControllerBase
 {
+    ICurriculumVitaeServices cvService { get; set; }
+    public CurriculumVitaeController(ICurriculumVitaeServices cvService)
+    {
+        this.cvService = cvService;
+    }
+    [HttpGet]
+    public async Task<Results<Ok<IEnumerable<Profiel>>, BadRequest<string>>> GetAllProfiles()
+    {        
+        try
+        {
+            var data = await cvService.GetAllProfiles();
+            if (data == null)
+            {
+                return TypedResults.BadRequest("Null pointer");
+            }
+            return TypedResults.Ok(data);
+        } catch(Exception ex)
+        {
+            return TypedResults.BadRequest("Could not execute internal code");
+        } finally
+        {
+            
+        }
+        
+        
+        
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using CurriculumVitae.common.Services;
+
 namespace CurriculumVitae.API
 {
     public class Program
@@ -8,7 +10,7 @@ namespace CurriculumVitae.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddScoped<ICurriculumVitaeServices,CurriculumVitaeServices>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
